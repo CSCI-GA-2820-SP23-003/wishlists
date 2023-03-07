7413a83e3235c63be5ade9e31d92823d07248d91
So Hyun Chi <sc9120@nyu.edu>
2023-03-07 16:49:52 +0000

    init commit

--- tests/test_items_model.py
+++ tests/test_items_model.py
@@ -54,7 +54,7 @@ class TestItemsModel(unittest.TestCase):
     ######################################################################
 
     def test_create_item(self):
-        """Creates an Item and asserts that it exists."""
+        """Creates an Item and asserts that it exist."""
         item = Item(product_name="first item", wishlist_id=1, item_quantity=1)
         self.assertEqual(str(item), "<Item first item id=[None]>")
         self.assertTrue(item is not None)
