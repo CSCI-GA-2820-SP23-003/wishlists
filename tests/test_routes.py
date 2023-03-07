72af115d618ea43c2e37cd21f429f94a367de4ed
Reuben Abraham <reuben.abraham20@gmail.com>
2023-03-07 21:54:23 +0000

    Skeleton Create and Read endpoints with basic testing

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -55,6 +55,20 @@ class TestWishlistService(TestCase):
         """ This runs after each test """
         db.session.remove()
 
+    def __create_wishlists(self, count):
+        """Factory method to create wishlists in bulk"""
+        wishlists = []
+        for _ in range(count):
+            test_wishlist = WishlistsFactory()
+            response = self.app.post(BASE_URL, json=test_wishlist.serialize())
+            self.assertEqual(
+                response.status_code, status.HTTP_201_CREATED, "Could not create test order"
+            )
+            new_wishlist = response.get_json()
+            test_wishlist.id = new_wishlist["id"]
+            wishlists.append(test_wishlist)
+        return wishlists
+
     ######################################################################
     #  P L A C E   T E S T   C A S E S   H E R E
     ######################################################################
@@ -63,6 +77,22 @@ class TestWishlistService(TestCase):
         """ It should call the home page """
         resp = self.app.get("/")
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
+
+    def test_get_wishlist(self):
+        """It should Read a single Wishlist"""
+        # get the id of a order
+        test_wishlist = self.__create_wishlists(1)[0]
+        response = self.app.get(f"{BASE_URL}/{test_wishlist.id}")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        data = response.get_json()
+        self.assertEqual(data["id"], test_wishlist.id)
+
+    def test_get_wishlist_not_found(self):
+        """It should not Read a Wishlist thats not found"""
+        response = self.app.get(f"{BASE_URL}/0")
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+        data = response.get_json()
+        self.assertIn("was not found", data["message"])
 
 
 ######################################################################
