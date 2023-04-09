888a5b944acfd24a87507e795a95b178c137670c
Saksham Garg <sg6482@nyu.edu>
2023-04-09 09:13:20 -0400

    Initial setup for BDD testing
    Signed-off-by: Saksham Garg <sg6482@nyu.edu>

---
+++ features/steps/wishlists_steps.py
@@ -0,0 +1,9 @@
+"""
+Wishlist Steps
+Steps file for Wishlist.feature
+For information on Waiting until elements are present in the HTML see:
+    https://selenium-python.readthedocs.io/waits.html
+"""
+import requests
+from behave import given
+from compare import expect
