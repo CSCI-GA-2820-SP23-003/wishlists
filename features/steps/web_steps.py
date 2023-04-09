e995b238f75382e2028bbd1454fd63c83a9a7a68
Saksham Garg <sg6482@nyu.edu>
2023-04-09 09:38:53 -0400

    Fix linting errors
    Signed-off-by: Saksham Garg <sg6482@nyu.edu>

--- features/steps/web_steps.py
+++ features/steps/web_steps.py
@@ -6,11 +6,3 @@ Steps file for web interactions with Selenium
 For information on Waiting until elements are present in the HTML see:
     https://selenium-python.readthedocs.io/waits.html
 """
-import logging
-from behave import when, then
-from compare import expect, ensure
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import Select, WebDriverWait
-from selenium.webdriver.support import expected_conditions
-
-ID_PREFIX = 'wishlist_'
