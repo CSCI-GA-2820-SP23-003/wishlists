e06e48a36ef09fa09aa2790e9e45f595b970b419
fakhruddinpattan <99637206+fakhruddinpattan@users.noreply.github.com>
2023-04-10 08:22:09 -0400

    UI skeleton for wishlist service (#78)

--- service/routes.py
+++ service/routes.py
@@ -26,17 +26,8 @@ def health():
 ######################################################################
 @app.route("/")
 def index():
-    """Root URL response"""
-    app.logger.info("Request for Root URL")
-
-    return (
-        jsonify(
-            name="Wishlists REST API Service",
-            version="1.0",
-            paths=url_for("list_wishlists", _external=True),
-        ),
-        status.HTTP_200_OK,
-    )
+    """Base URL for our service"""
+    return app.send_static_file("index.html")
 
 ######################################################################
 #  R E S T   A P I   E N D P O I N T S
